4d2ae7f27c9cac0415a45f593a929acd8ef09f2f
malatofu <956975753@qq.com>
2021-08-17 11:25:20 +0800

    Optimize the structure

--- shell/kill_camera.sh
+++ shell/kill_camera.sh
@@ -2,7 +2,7 @@
 
 proc=`ps aux|grep "/jydz/display/main"|grep -v "grep"|awk '{print $2}'`
 echo $proc
-echo root|sudo -S kill $proc
+echo linaro|sudo -S kill $proc
 
 sh /jydz/shell/start.sh &
 
--- shell/kill_process.sh
+++ shell/kill_process.sh
@@ -2,10 +2,10 @@
 
 proc=`ps aux|grep "/jydz/shell/start.sh"|grep -v "grep"|awk '{print $2}'`
 echo $proc
-echo root|sudo -S kill $proc
+echo linaro|sudo -S kill $proc
 proc1=`ps aux|grep "/jydz/bin/main"|grep -v "grep"|awk '{print $2}'`
 echo $proc1
-echo root|sudo -S kill $proc1
+echo linaro|sudo -S kill $proc1
 #proc2=`ps aux|grep "MyModel_separate"|grep -v "grep"|awk '{print $2}'`
 #echo $proc2
 #echo root|sudo -S kill $proc2
